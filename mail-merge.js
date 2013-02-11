ba65f797989b20be20becf493cc90b7f3c2c5c26
Kyle Kellogg <kyle@kylekellogg.com>
2013-02-10 20:15:56 -0500

    Updated with some documentation

--- mail-merge.js
+++ mail-merge.js
@@ -3,6 +3,12 @@
 var fs = require( 'fs' );
 
 var Template = (function createTemplate() {
+  /**
+  * Object to represent variable, filter, and format holders in the template
+  * 
+  * @arg index int Offset from beginning of template.
+  * @arg data String The complete text of the holder.
+  */
   function Piece( index, data ) {
     var self = this;
     var targetKeyRegExp = /(\{(?!#)([^\|]+)\|(?:f(?:ilter|ormat)\:)([^\}]+)\})/ig;
@@ -10,6 +16,11 @@ var Template = (function createTemplate() {
     var filterRegExp = /filter/i;
     var formatRegExp = /format/i;
     
+    /**
+    * Apply filter to raw input
+    * 
+    * @arg raw String Raw input.
+    */
     function filter( raw ) {
       switch( self.target.toLowerCase() ) {
         case 'lowercase':
@@ -19,6 +30,11 @@ var Template = (function createTemplate() {
       }
     }
     
+    /**
+    * Apply format to raw input.
+    * 
+    * @arg raw String Raw input.
+    */
     function format( raw ) {
       switch ( self.target.toLowerCase() ) {
         case 'date':
@@ -31,6 +47,15 @@ var Template = (function createTemplate() {
       }
     }
     
+    /**
+    * Publicly accessible variables
+    * 
+    * @var index int The offset from the beginning of the template.
+    * @var rawData String The original and complete holder's text.
+    * @var type String The type of the Piece object. Either 'filter', 'format', or 'var'.
+    * @var target String The filter or format to be applied. Will be 'none' for type 'var'.
+    * @var key String The name of the variable to use to replace this holder's text with upon export.
+    */
     this.index = index;
     this.rawData = data;
     
@@ -38,6 +63,11 @@ var Template = (function createTemplate() {
     this.target = filterFormatRegExp.test( this.type ) ? data.replace( targetKeyRegExp, '$3' ) : 'none';
     this.key = filterFormatRegExp.test( this.type ) ? data.replace( targetKeyRegExp, '$2' ) : data.replace( /\{(?!#)([^\}]+)\}/, '$1' );
     
+    /**
+    /* Generate and return the end value for this Piece
+    /* 
+    /* @arg raw String The raw data to use.
+    /*/
     this.returnValue = function returnValue( raw ) {
       if ( filterRegExp.test( self.type ) ) {
         return filter( raw );
@@ -49,10 +79,23 @@ var Template = (function createTemplate() {
     };
   }
   
+  /**
+  * Object to represent conditional blocks in the template
+  * 
+  * @arg index int Offset from beginning of template.
+  * @arg data String The complete text of the holder.
+  */
   function Block( index, data ) {
     var self = this;
     var conditionalKeyValueRegExp = /\{#(if|else(?:\sif)?)(?:\s([^\}]+))?\}[\r\n]((?:[^\{]+))/gi;
     
+    /**
+    * Publicly accessible variables
+    * 
+    * @var index int The offset from the beginning of the template.
+    * @var rawData String The original and complete holder's text.
+    * @var conditions Array The list of conditions to test for in this Block
+    */
     this.index = index;
     this.rawData = data;
     
@@ -74,6 +117,11 @@ var Template = (function createTemplate() {
       return 0;
     } );
     
+    /**
+    /* Generate and return the end value for this Block
+    /* 
+    /* @arg keys Object The key value map, in the format key[ name ] = value.
+    /*/
     this.returnValue = function returnValue( keys ) {
       var elseRegExp = /else(?!\sif)/i;
       for ( var i = 0, l = self.conditions.length; i < l; i++ ) {
@@ -90,6 +138,10 @@ var Template = (function createTemplate() {
     };
   }
   
+  /**
+  * Internal type of Template. Has all functionality and capabilities of Template as
+  * it's used to return a fully instantiated object to the Template var.
+  */
   function _Template() {
     var self = this;
     
@@ -104,12 +156,27 @@ var Template = (function createTemplate() {
       return 0;
     }
     
+    /**
+    * Publicly accessible variables
+    * 
+    * @var rawData String The original and complete holder's text.
+    * @var pieces Array The list of Piece objects for the template.
+    * @var blocks Array The list of Block objects for the template.
+    * @var outputDir String The path of the output directory.
+    */
     this.rawData = '';
     this.pieces = [];
     this.blocks = [];
     
     this.outputDir = '';
     
+    /**
+    /* Consume raw template data and find all the Piece objects that will be used.
+    /* Purposefully hold off from finding the Block objects until the Piece objects have
+    /* supplied their end values in order to prevent messy atomic operations.
+    /* 
+    /* @arg data String The raw template text.
+    /*/
     this.process = function process( data ) {
       var roughVarRegExp = /(\{(?!#)([^\}]+)\})/g;
       
@@ -123,6 +190,14 @@ var Template = (function createTemplate() {
       self.pieces.sort( sortOnIndex );
     };
     
+    /**
+    /* Using data file, export newly merged file.
+    /* After swapping all applicable Piece objects (from the end of the template to the beginning),
+    /* find and swap the Block objects.
+    /* 
+    /* @arg data String The raw data text.
+    /* @arg filename String The name of the file to be exported.
+    /*/
     this.exportUsingDataFile = function( data, filename ) {
       var keys = {length:0};
       var str = self.rawData;
@@ -181,11 +256,15 @@ var Template = (function createTemplate() {
 })();
 
 process.argv.forEach( function handleArguments( val, idx ) {
-  //  node
-  //  file.js
-  //  template
-  //  output dir
-  //  input dir
+  /**
+  * Format the arguments come in:
+  * 
+  * node
+  * filename.js
+  * template file
+  * output dir
+  * input dir
+  */
   
   var path = '';
   
